9a9eec5224a84c9e2372ae997800fdb372fe497a
Kir Proto Q <kir.proto@ya.ru>
2022-02-04 06:36:19 +0500

    Initial Commit

---
+++ Yan_SwiftUI_Text/Yan_SwiftUI_TextApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yan_SwiftUI_TextApp.swift
+//  Yan_SwiftUI_Text
+//
+//  Created by KIR Q on 2022.02.04.
+//
+
+import SwiftUI
+
+@main
+struct Yan_SwiftUI_TextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
